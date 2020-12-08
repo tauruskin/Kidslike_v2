b5166500ec426c4b699b7f32f6807bdead882bf3
Dmytro <tschernomord@gmail.com>
2020-12-08 09:19:32 +0100

    add link styles

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -2,18 +2,25 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 import pin from '../../images/header/pin.svg'
 import gift from '../../images/header/gift.svg'
+import styles from './Navigation.module.css'
 
 export default function Navigation() {
   return (<>
-    <NavLink to='/'>
-      <img src={pin} alt="pin" />
-        Звички i задачi
+    <ul className={styles.navList}>
+      <li className={styles.listItem}>
+        <img className={styles.itemImg} src={pin} alt="pin" />
+        <NavLink className={styles.navLink} to='/'>
+          Звички i задачi
+        </NavLink>
+      </li>
+      <li className={styles.listItem}>
+        <img className={styles.itemImg} src={gift} alt="gift" />
+        <NavLink className={styles.navLink} to='/'>
+          Подарунки
     </NavLink>
-    <NavLink to='/'>
-      <img src={gift} alt="gift" />
+      </li>
+    </ul>
 
-        Подарунки
-    </NavLink>
   </>
   )
 }
