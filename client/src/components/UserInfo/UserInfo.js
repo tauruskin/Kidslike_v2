05cf85d4a180db2edaed137298c61e98acf31d4b
Dmytro <tschernomord@gmail.com>
2020-12-08 08:44:52 +0100

    update css

--- client/src/components/UserInfo/UserInfo.js
+++ client/src/components/UserInfo/UserInfo.js
@@ -2,12 +2,15 @@ import React from 'react'
 import defaultLogo from '../../images/header/userInfo.svg'
 import logout from '../../images/header/logout.svg'
 import style from './UserInfo.module.css'
+import { NavLink } from 'react-router-dom'
 export default function UserInfo() {
   return (
     <>
       <img src={defaultLogo} alt="default logo" />
       <span className={style.userName} >Name</span>
-      <img src={logout} alt='logout' className={style.logout} />
+      <NavLink to='/'>
+        <img src={logout} alt='logout' className={style.logout} />
+      </NavLink>
     </>
   )
 }
