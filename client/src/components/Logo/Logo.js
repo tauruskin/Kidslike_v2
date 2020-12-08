cf6005fcbf1b13a7220ba1fe40ec7e43d606115b
Dmytro <tschernomord@gmail.com>
2020-12-08 08:39:06 +0100

    Update Logo.js

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import logo from '../../images/header/logo.svg'
 import styles from './Logo.module.css'
 
 export default function Logo() {
     return (
         <>
-            <img className={styles.siteLogo} src={logo} alt='logo' />
+            <NavLink to='/'>
+                <img className={styles.siteLogo} src={logo} alt='logo' />
+            </NavLink>
             <span className={styles.logoText}><span className={styles.bold}>Kidslike</span> v2</span>
         </>
     )
