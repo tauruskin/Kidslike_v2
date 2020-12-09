687ea192260aceddc90d2c0d6064a1d1bc35f82d
ihor <malishkoio@gmail.com>
2020-12-09 23:03:44 +0200

    remove LeftSideBar

--- client/src/Views/HomeView.js
+++ client/src/Views/HomeView.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import LeftSideBar from '../components/LeftSideBar/LeftSideBar';
 
 class HomeView extends Component {
   render() {
     return (
       <>
         <h2>Home</h2>
+        <LeftSideBar />
       </>
     );
   }
--- client/src/components/PublicRoute.js
+++ client/src/components/PublicRoute.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import LeftSideBar from './LeftSideBar/LeftSideBar';
 
 const PublicRoute = ({
   component: Component,
   isAuthenticated,
   ...routeProps
 }) => (
-    <>
-      <LeftSideBar />
-      <Route
-        {...routeProps}
-        render={props =>
-          isAuthenticated ? <Redirect to="/home" /> : <Component {...props} />
-        }
-      /></>
-  );
+  <>
+    <Route
+      {...routeProps}
+      render={props =>
+        isAuthenticated ? <Redirect to="/home" /> : <Component {...props} />
+      }
+    />
+  </>
+);
 
 export default PublicRoute;
