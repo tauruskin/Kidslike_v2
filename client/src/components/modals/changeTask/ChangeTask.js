4461da6a7cdbf56a918eedb263b778e887e83c16
Tofik <smilokiller@mail.ru>
2020-12-10 15:44:15 +0200

    getting info from forms

--- client/src/components/modals/changeTask/ChangeTask.js
+++ client/src/components/modals/changeTask/ChangeTask.js
@@ -4,22 +4,51 @@ import styles from './ChangeTask.module.css';
 import modalBackDrop from '../../modalBackDrop/ModalBackDrop';
 
 const ChangeTask = ({ close }) => {
+  const [taskName, setTaskName] = useState('');
+  const [mark, setMark] = useState('');
+  const [taskTarget, setTaskTarget] = useState('');
+  const [taskDays, setTaskDays] = useState('');
+
+  const handleSubmit = evt => {
+    console.log(
+      'taskName:',
+      taskName,
+      'mark:',
+      mark,
+      'taskTarget',
+      taskTarget,
+      'taskDays',
+      taskDays,
+    );
+    evt.preventDefault();
+    close();
+  };
+
+  const handleDelete = () => {
+    close();
+  };
+
   return (
     <>
       <div className={styles.modalBody}>
         <h2 className={styles.title}>Редагування задачі</h2>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
           <div className={styles.inputBlock}>
             <label className={styles.label}>
               <p className={styles.inputName}>Назва</p>
               <input
                 className={styles.input}
+                onChange={({ target: { value } }) => setTaskName(value)}
                 placeholder="Введіть назву"
               ></input>
             </label>
             <label className={styles.label}>
               <p className={styles.inputName}>Призначення звички</p>
-              <select className={styles.select} placeholder="Оберіть дитину">
+              <select
+                className={styles.select}
+                onChange={({ target: { value } }) => setTaskTarget(value)}
+                placeholder="Оберіть дитину"
+              >
                 <option disabled>Выберите героя</option>
                 <option value="Чебурашка">Чебурашка</option>
                 <option value="Крокодил Гена">Крокодил Гена</option>
@@ -30,16 +59,24 @@ const ChangeTask = ({ close }) => {
             <label className={styles.label}>
               <p className={styles.inputName}>Бал</p>
 
-              <input className={styles.inputMark} placeholder="__"></input>
+              <input
+                className={styles.inputMark}
+                onChange={({ target: { value } }) => setMark(value)}
+                placeholder="__"
+              ></input>
             </label>
             <label className={styles.label}>
               <p className={styles.inputName}>
                 Дні на виконання (необов’язково)
               </p>
 
-              <input className={styles.inputDays} placeholder="___"></input>
+              <input
+                className={styles.inputDays}
+                onChange={({ target: { value } }) => setTaskDays(value)}
+                placeholder="___"
+              ></input>
             </label>
-            <button className={styles.btnDelete}>
+            <button className={styles.btnDelete} onClick={() => handleDelete()}>
               <span className={styles.btnDeleteIcon}></span> Видалити задачу
             </button>
           </div>
