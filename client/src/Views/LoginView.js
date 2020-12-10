22294ea9154e68387de51790297c902fdf7ed6af
Elena Shevchenko <elena.kadru@gmail.com>
2020-12-10 20:54:57 +0200

    added first textfield

--- client/src/Views/LoginView.js
+++ client/src/Views/LoginView.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import { AddFormPresent } from '../components/AddFormPresent/AddFormPresent';
 
 export default class LoginView extends Component {
   render() {
-    return <div>LoginView</div>;
+    return (
+      <>
+        <div>LoginView</div>
+        <AddFormPresent />
+      </>
+    );
   }
 }
