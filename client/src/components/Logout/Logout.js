80608bbb92b3f38fa042cada0ffdd4af86e458e3
Koval <dmytrokovalskyi@yahoo.com>
2020-12-11 17:44:51 +0200

    modal Styles and Routes Fixed

--- client/src/components/Logout/Logout.js
+++ client/src/components/Logout/Logout.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import styles from './logout.module.css';
 import modalBackDrop from '../modalBackDrop/ModalBackDrop';
-import moduleName from '../UIcomponents/Button/Button';
-// import { orange } from '@material-ui/core/colors';
-const { Container, Text, Button, ButtonWrapper } = styles;
+import Button from '../UIcomponents/Button/Button';
+import { NavLink } from 'react-router-dom';
+
+const { Container, Text, btn, ButtonWrapper } = styles;
 const Logout = ({ close }) => {
   return (
     <>
       <div className={Container}>
         <p className={Text}>Ви впевненi що хочете вийти?</p>
         <div className={ButtonWrapper}>
-          {/* <button className={Button} onClick={() => close()}>
-            Так
-          </button> */}
-          <Button className={('#ff9800', Button)} onClick={() => close()}>
-            Так
-          </Button>
-          <Button className={('#ff9800', Button)} onClick={() => close()}>
-            Ні
-          </Button>
-          {/* <button className={Button} onClick={() => close()}>
-           
-          </button> */}
+          <NavLink to="/">
+            <Button
+              orange={true}
+              type="button"
+              className={btn}
+              handleClick={() => close()}
+              label="Так"
+            />
+          </NavLink>
+
+          <Button
+            orange={true}
+            type="button"
+            className={btn}
+            handleClick={() => close()}
+            label="Hi"
+          />
         </div>
       </div>
     </>
--- client/src/components/UserInfo/UserInfo.js
+++ client/src/components/UserInfo/UserInfo.js
@@ -1,44 +1,35 @@
-import React, {Component, useState} from 'react';
+import React, { Component, useState } from 'react';
 import defaultLogo from '../../img/header/userInfo.svg';
 import logout from '../../img/header/logout.svg';
 import style from './UserInfo.module.css';
-import { NavLink } from 'react-router-dom';
-// import Logout from '../Logout/Logout';
-// import LogoutModal from '../Logout/LogoutModal'
-import Logout from '../Logout/Logout'
+import Logout from '../Logout/Logout';
 
+export default function UserInfo() {
 
-export default function UserInfo()  {
-  // state = {
-  //   isModalOpen: false,
-  // };
-  // handleOpenModal = () => {
-  //   this.setState({ isModalOpen: true });
-  // };
-  // handleCloseModal = () => {
-  //   this.setState({ isModalOpen: false });
-  const [showModal, setShowModal] = useState(false)
-  const close = () => {setShowModal(false)}
+  const [showModal, setShowModal] = useState(false);
+  const close = () => {
+    setShowModal(false);
+  };
 
+  return (
+    <>
+      <div className={style.userInfoContainer}>
+        <img
+          className={style.userAvatar}
+          src={defaultLogo}
+          alt="default logo"
+        />
+        <span className={style.userName}>Name</span>
 
+        <img
+          src={logout}
+          alt="logout"
+          className={style.logout}
+          onClick={() => setShowModal(true)}
+        />
 
-  
- 
-    return (
-      <>
-        <div className={style.userInfoContainer}>
-          <img
-            className={style.userAvatar}
-            src={defaultLogo}
-            alt="default logo"
-          />
-          <span className={style.userName}>Name</span>
-          <NavLink to="/">
-            <img src={logout} alt="logout" className={style.logout} onClick={() => setShowModal(true)} />
-          </NavLink>
-          {showModal && <Logout close={() => close()}/>}
-        </div>
-      </>
-    );
-
+        {showModal && <Logout close={() => close()} />}
+      </div>
+    </>
+  );
 }
