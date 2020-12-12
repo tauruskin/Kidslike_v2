43ea374fead6059f057295877c1641319d1fee93
Nataliia <n3v3st4@gmail.com>
2020-12-12 21:51:28 +0200

    redux-habit actions & reducer

---
+++ client/src/redux/habbit/habbitOperations.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+import actions from './habbitActions';
+
+axios.defaults.baseURL = 'http://kidslike-v2.top/';
+
--- client/src/redux/habbit/habbitReducer.js
+++ client/src/redux/habbit/habbitReducer.js
@@ -1,10 +1,21 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { createHabbit, deleteHabbit, updateHabbit } from './habbitActions';
+import actions from './habbitActions';
 
-const userHabitsState = [];
+const userHabitsInitialState = {
+  name: 'Test Habit #1',
+  childId: '5fd16a4cf5208f36bc9ca42b',
+  points: 5,
+  daysToComplete: [true, true, null, null, false, false, false, false, false, false],
+  createdAt: '2020-12-10T09:47:43.152+00:00',
+  updatedAt: '2020-12-12T10:41:05.180+00:00',
+};
 
-const userHabits = createReducer(userHabitsState, {
-  [createHabbit]: (state, { payload }) => [
+const userHabits = createReducer(userHabitsInitialState, {
+  [actions.getAllHabitsSuccess]: (_, { payload }) => {
+    return {...payload.habits};
+  },
+
+  [actions.createHabbitSuccess]: (state, { payload }) => [
     ...state,
 
     {
@@ -16,7 +27,9 @@ const userHabits = createReducer(userHabitsState, {
       updatedAt: payload.updatedAt,
     },
   ],
-  [updateHabbit]: (state, { payload }) =>
+  [actions.updateHabbitSuccess]: (state, { payload }) =>
     state.map(el => (el._id === payload._id ? (el = payload) : el)),
+  [actions.deleteHabbitSuccess]: (state, { payload }) =>
+    state.filter(habit => habit._id !== payload),
 });
 export default userHabits;
