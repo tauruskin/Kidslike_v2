99f7339a92f8eb78510f96f5684c6e56c3d3998d
ihor <malishkoio@gmail.com>
2020-12-12 11:13:30 +0200

    fix authorize to all

--- habits/habits.router.js
+++ habits/habits.router.js
@@ -17,32 +17,22 @@ const { authorize } = require("../helpers/auth/token_verify");
 
 const router = Router();
 
+router.all(authorize);
 // CRUD
 
 // 1. C - Create
-router.post(
-  "/",
-  authorize,
-  validate(CreateHabitSchema),
-  asyncWrapper(createHabit)
-);
+router.post("/", validate(CreateHabitSchema), asyncWrapper(createHabit));
 
 // 2. R - Read
 router.get("/", asyncWrapper(getHabits));
 router.get("/:id", asyncWrapper(getHabitById));
 
 // // 3. U - Update
-router.patch(
-  "/:id",
-  authorize,
-  validate(UpdateHabitSchema),
-  asyncWrapper(updateHabit)
-);
+router.patch("/:id", validate(UpdateHabitSchema), asyncWrapper(updateHabit));
 
 // 4. D - Delete
 router.delete(
   "/:id",
-  authorize,
   validate(validateIdSchema, "params"),
   asyncWrapper(deleteHabit)
 );
