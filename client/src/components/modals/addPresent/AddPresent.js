914e8733f1b48e09d602c2dfdc8de17a47113c82
Elena Shevchenko <elena.kadru@gmail.com>
2020-12-12 20:17:15 +0200

    implemented AddPresent form component with styles

--- client/src/components/modals/addPresent/AddPresent.js
+++ client/src/components/modals/addPresent/AddPresent.js
@@ -4,13 +4,17 @@ import Button from '../../UIcomponents/Button/Button';
 import { BasicInput } from '../../UIcomponents/Input/BasicInput';
 import { CustomSelect } from '../../UIcomponents/CustomSelect/CustomSelect';
 import modalBackDrop from '../../modalBackDrop/ModalBackDrop';
+import { MarkInput } from '../../UIcomponents/MarkInput/MarkInput';
 
 function AddPresent({ close }) {
   //const dispatch = useDispatch();
   const [presentTitle, setPresentTitle] = useState('');
   const [mark, setMark] = useState('');
   const [childName, setChildName] = useState('Оберіть дитину');
-  const [img, setImg] = useState('');
+  const [file, setFile] = useState('');
+  const [fileName, setFileName] = useState('');
+  const [fileError, setFileError] = useState('');
+  const fileInput = React.createRef();
 
   const handleTitleChange = event => {
     setPresentTitle(event.target.value);
@@ -19,49 +23,87 @@ function AddPresent({ close }) {
   const handleNameChange = event => {
     setChildName(event.target.value);
   };
+  const handleMarkChange = event => {
+    setMark(event.target.value);
+  };
+  const handleFileChange = () => {
+    const imgFile = fileInput.current.files[0];
+    const reader = new FileReader();
+
+    if (imgFile) {
+      if (imgFile.size > 16777000) {
+        setFileError('Файл завеликий');
+        return;
+      }
+      setFileError('');
+      setFileName(imgFile.name)
+      reader.readAsDataURL(imgFile);
+      reader.onloadend = function () {
+        setFile(reader.result);
+      };
+    }
+  };
   const resetState = () => {
     setPresentTitle('');
     setChildName('Оберіть дитину');
     setMark('');
-    setImg('');
+    setFile('');
   };
 
   const handleSubmit = event => {
     console.log('presentTitle:', presentTitle);
     console.log('mark:', mark);
     console.log('childName', childName);
-    console.log('img', img);
-
+    console.log('file', file);
     event.preventDefault();
     //dispatch logic
-    resetState()
+    resetState();
     close();
   };
 
   return (
-    <div className={styles.Container}>
+    <div className={styles.ModalBody}>
       <div className={styles.FormWrap}>
         <h3 className={styles.Title}>Додавання подарунку</h3>
         <form onSubmit={handleSubmit} autoComplete="off">
-          <BasicInput
-            forLabel={'presentTitle'}
-            id={'presentTitle'}
-            labelText={'Назва'}
-            value={presentTitle}
-            name="presentTitle"
-            handleChange={handleTitleChange}
-            placeholder={'Введіть назву'}
-            inputWidth={340}
-            type="text"
-          />
-          <CustomSelect
-            id={'childName'}
-            name={'childName'}
-            labelText={'Призначення подарунку'}
-            handleChange={handleNameChange}
-            value={childName}
-          />
-          <div className={styles.Wrapper}>
+          <div className={styles.InputsWrapper}>
+            <BasicInput
+              forLabel={'presentTitle'}
+              id={'presentTitle'}
+              labelText={'Назва'}
+              value={presentTitle}
+              name="presentTitle"
+              handleChange={handleTitleChange}
+              placeholder={'Введіть назву'}
+              type="text"
+            />
+            <CustomSelect
+              id={'childName'}
+              name={'childName'}
+              labelText={'Призначення подарунку'}
+              handleChange={handleNameChange}
+              value={childName}
+            />
+            <MarkInput value={mark} onChange={handleMarkChange} /> 
+            <div>
+              <span className={styles.FileInputTitle}>
+                Завантажити фото (необов’язково)
+              </span>
+              <label htmlFor="file" className={styles.FileInputLabel}>
+                {fileError ? fileError : file ? fileName : "Оберіть файл"}
+              
+              <input
+                type="file"
+                accept="image/*"
+                name="file"
+                id="file"
+                ref={fileInput}
+                onChange={handleFileChange}
+                className={styles.FileInput}
+              /></label>
+            </div>
+          </div>
+          <div className={styles.ButtonsBlock}>
             <Button
               type="button"
               handleClick={close}
@@ -76,6 +118,10 @@ function AddPresent({ close }) {
             />
           </div>
         </form>
+        <button
+          onClick={() => close()}
+          className={styles.ModalCloseBtn}
+        ></button>
       </div>
     </div>
   );
