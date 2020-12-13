4cb913a416db444565f3ae3a52e2a7419d5f7610
Nataliia <n3v3st4@gmail.com>
2020-12-13 12:43:35 +0200

    HomeView componentDidMount

--- client/src/Views/HomeView.js
+++ client/src/Views/HomeView.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import LeftSideBar from '../components/LeftSideBar/LeftSideBar';
 import GiftsView from './GiftsView';
 import ChildTaskPage from '../components/ChildTaskPage/ChildTaskPage';
 import HabitsList from '../components/HabitsList/HabitsList';
 import TaskList from '../components/TaskList/TaskList';
 import Container from '../components/Container/Container';
+import authOperations from '../redux/auth/authOperations';
+import authSelector from '../redux/auth/authSelectors';
+import habbitOperations from '../redux/habbit/habbitOperations';
 
 class HomeView extends Component {
+
+  componentDidMount() {
+    // console.log(this.props.token);
+    // authOperations.setToken(this.props.token);
+    this.props.getAllHabits({children: ['5fd49e403e33eb3eda2f2af5']});
+  }
+
   render() {
     const { match } = this.props;
     return (
@@ -37,4 +48,11 @@ class HomeView extends Component {
   }
 }
 
-export default HomeView;
+const mapStateToProps = state => ({
+  // token: authSelector.isAuthenticated(state),
+});
+
+export default connect(mapStateToProps, {
+  // onLogOut: authOperations.logOut,
+  getAllHabits: habbitOperations.getAllHabits,
+})(HomeView);
