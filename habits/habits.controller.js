2d9588c1b4d7fca7ba2e70745f19771094a7ace1
Nataliia <n3v3st4@gmail.com>
2020-12-14 23:39:00 +0200

    changes habit model and schema in db

--- habits/habits.controller.js
+++ habits/habits.controller.js
@@ -2,24 +2,21 @@ const { HabitModel } = require("./habits.model");
 
 exports.createHabit = async (req, res, next) => {
   const newHabit = await HabitModel.create(req.body);
+  console.log(newHabit);
   return res.status(201).send(newHabit);
 };
 
 exports.getHabits = async (req, res, next) => {
-  const { childId, tokens, username } = req.user;
-  console.log(req.user);
-  console.log('childId--', childId);
-  console.log('tokens--', tokens);
-  console.log('username--', username);
+  const { childId} = req.user;
   const habits = await HabitModel.find({ childId: childId });
   // const { childId } = req.user;
   // const habits = await HabitModel.find({ childId: childId  });
   return res.status(200).send(habits);
 };
 
-exports.getHabitById = async (req, res, next) => {
-  return res.status(200).send(req.hebit);
-};
+// exports.getHabitById = async (req, res, next) => {
+//   return res.status(200).send(req.hebit);
+// };
 
 exports.updateHabit = async (req, res, next) => {
   const { _id } = req.habit;
@@ -29,11 +26,18 @@ exports.updateHabit = async (req, res, next) => {
   return res.status(200).send(updatedHabit);
 };
 
+exports.checkHabitDone = async (req, res, next) => {
+  const { _id } = req.habit;
+  const habitToCheck = await HabitModel.findOneAndUpdate({ _id, 'daysToComplete.date': req.body.date },
+    { $set: { 'daysToComplete.$.done': req.body.done } }, {
+    new: true,
+  });
+  return res.status(200).send(habitToCheck);
+};
+
 exports.deleteHabit = async (req, res, next) => {
   const { _id } = req.habit;
-  // console.log(Object.keys(req));
-  // console.log(req.habit);
   await HabitModel.findByIdAndDelete(_id);
 
-  return res.status(204).send();
+  return res.status(204).send(_id);
 };
