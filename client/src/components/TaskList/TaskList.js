698ac55e55ec34605d4d28cb52a84b0b8043c225
ihor <malishkoio@gmail.com>
2020-12-15 15:38:08 +0200

    formatting text

--- client/src/components/TaskList/TaskList.js
+++ client/src/components/TaskList/TaskList.js
@@ -1,43 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import TaskItem from '../TaskItem/TaskItem';
 import { useSelector, useDispatch } from 'react-redux';
-import operations from '../../redux/tasks/taskOperations'
-import styles from './TaskList.module.css'
-import taskTemplate from './taskTemplate.json'
+import operations from '../../redux/tasks/taskOperations';
+import styles from './TaskList.module.css';
+import taskTemplate from './taskTemplate.json';
 import MoreButton from '../UIcomponents/MoreButton/MoreButton';
 import Button from '../UIcomponents/Button/Button';
 import AddTask from '../modals/addTask/AddTask';
 
 export default function TaskList() {
-    const [showAddTaskModal, setShowAddTaskModal] = useState(false);
-    const close = () => { setShowAddTaskModal(false) };
-    const tasks = useSelector(state => state.tasks)
-    const dispatch = useDispatch();
-    useEffect(() => {
-       dispatch(operations.getAllTasks())
-    }, [])
-    useEffect(() => {
-        
-    }, [tasks])
-    return (
-        <div className={styles.listHolder}>
-            <div className={styles.gifsTitleContainer}>
-                <div className={styles.giftIcon}></div>
-                <h1 className={styles.giftTitle}>Задачі</h1>
-            </div>
-            <ul className={styles.HabitList}>
-                {tasks.map(el => (
-                    <li 
-                        key={el._id}
-                        className={styles.HabitItem}
-                    >
-                        <MoreButton type={'task'} data={el} />
-                        <TaskItem  {...el} />
-                    </li>
-                ))}
-            </ul>
-            <Button label={'Додати задачу +'} handleClick={() => setShowAddTaskModal(true)} type={'button'} orange={true} />
-            {showAddTaskModal && <AddTask close={() => close()} />}
-        </div>
-    )
+  const [showAddTaskModal, setShowAddTaskModal] = useState(false);
+  const close = () => {
+    setShowAddTaskModal(false);
+  };
+  const tasks = useSelector(state => state.tasks);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(operations.getAllTasks());
+  }, []);
+  useEffect(() => {}, [tasks]);
+  return (
+    <div className={styles.listHolder}>
+      <div className={styles.gifsTitleContainer}>
+        <div className={styles.giftIcon}></div>
+        <h1 className={styles.giftTitle}>Задачі</h1>
+      </div>
+      <ul className={styles.HabitList}>
+        {tasks.map(el => (
+          <li key={el._id} className={styles.HabitItem}>
+            <MoreButton type={'task'} data={el} />
+            <TaskItem {...el} />
+          </li>
+        ))}
+      </ul>
+      <Button
+        label={'Додати задачу +'}
+        handleClick={() => setShowAddTaskModal(true)}
+        type={'button'}
+        orange={true}
+      />
+      {showAddTaskModal && <AddTask close={() => close()} />}
+    </div>
+  );
 }
