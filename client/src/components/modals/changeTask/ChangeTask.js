a351131aeb4b0a6b9b818562a9c468c335c58a1e
Koval <dmytrokovalskyi@yahoo.com>
2020-12-15 14:37:08 +0200

    update& deletetask
    пока изменяются таски только если все поля ввести. нужно будет эту часть исправить

--- client/src/components/modals/changeTask/ChangeTask.js
+++ client/src/components/modals/changeTask/ChangeTask.js
@@ -5,26 +5,38 @@ import modalBackDrop from '../../modalBackDrop/ModalBackDrop';
 import operations from '../../../redux/tasks/taskOperations';
 import { useDispatch,useSelector } from 'react-redux';
 const ChangeTask = ({ close, data }) => {
-  console.log(data)
   const children = useSelector(state => state.children);
   const dispatch = useDispatch();
   const [taskName, setTaskName] = useState(data.name);
-  const [mark, setMark] = useState('');
-  const [taskTarget, setTaskTarget] = useState(data.ChildId);
+  const [mark, setMark] = useState(data.points);
+  const [taskTarget, setTaskTarget] = useState(data.childId);
   const [taskDays, setTaskDays] = useState(data.daysToComplete);
 
   const handleSubmit = evt => {
- 
+    console.log(
+      'name:',
+      taskName,
+      'points',
+      mark,
+      'id',
+      taskTarget,
+
+    );
+
+    dispatch(
+      operations.updateTask(
+        {
+          name: taskName,
+          childId: taskTarget,
+          points: mark,
+          daysToComplete: taskDays,
+        },
+        data._id,
+      ),
+    );
     evt.preventDefault();
     close();
-  };
-   dispatch(
-     operations.updateTask(
-       { name: taskName, childId: taskTarget, points: mark },
-       data._id,
-     ),
-   );
-
+  }
   const handleDelete = () => {
     dispatch(operations.deleteTask(data._id))
     close();
@@ -39,6 +51,7 @@ const ChangeTask = ({ close, data }) => {
             <label className={styles.label}>
               <p className={styles.inputName}>Назва</p>
               <input
+                defaultValue={data.name}
                 className={styles.input}
                 onChange={({ target: { value } }) => setTaskName(value)}
                 placeholder="Введіть назву"
@@ -47,11 +60,13 @@ const ChangeTask = ({ close, data }) => {
             <label className={styles.label}>
               <p className={styles.inputName}>Призначення звички</p>
               <select
+                defaultValue={data.childId}
                 value={children.childId}
                 className={styles.select}
                 onChange={({ target: { value } }) => setTaskTarget(value)}
                 placeholder="Оберіть дитину"
               >
+                <option key={children.id}>Оберіть дитину</option>
                 {children &&
                   children.map(child => (
                     <option key={child._id} value={child._id}>
@@ -64,6 +79,7 @@ const ChangeTask = ({ close, data }) => {
               <p className={styles.inputName}>Бал</p>
 
               <input
+                defaultValue={data.points}
                 className={styles.inputMark}
                 onChange={({ target: { value } }) => setMark(value)}
                 placeholder="__"
@@ -75,6 +91,7 @@ const ChangeTask = ({ close, data }) => {
               </p>
 
               <input
+                defaultValue={data.daysToComplete}
                 className={styles.inputDays}
                 onChange={({ target: { value } }) => setTaskDays(value)}
                 placeholder="___"
@@ -85,7 +102,9 @@ const ChangeTask = ({ close, data }) => {
             </button>
           </div>
           <div className={styles.buttonsBlock}>
-            <button onClick={handleSubmit} className={styles.buttonSave}>Зберегти</button>
+            <button  className={styles.buttonSave}>
+              Зберегти
+            </button>
 
             <button className={styles.buttonCancle} onClick={() => close()}>
               Відміна
