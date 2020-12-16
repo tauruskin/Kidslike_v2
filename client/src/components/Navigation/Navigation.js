f223f0cd49368767b34ded43411cbeacb27623d8
Dmytro <tschernomord@gmail.com>
2020-12-16 16:15:33 +0100

    fix active link

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -3,12 +3,16 @@ import { NavLink } from 'react-router-dom';
 import pin from '../../img/header/pin.svg';
 import gift from '../../img/header/gift.svg';
 import familyImg from '../../img/header/family.svg';
-
 import styles from './Navigation.module.css';
+import {useLocation} from "react-router-dom";
 
 export default function Navigation({ familyRender, family, familyRenderAnotherLinks }) {
+  const location = useLocation()
+  console.log(location.pathname);
+  const home = (location.pathname === "/home" && family === false)
+  const gifts = (location.pathname === "/home/gifts" && family === false)
 
-
+  console.log(home);
   return (
     <>
       <ul className={styles.navList}>
@@ -20,15 +24,15 @@ export default function Navigation({ familyRender, family, familyRenderAnotherLi
             </span>
           {/* </NavLink> */}
         </li>
-        <li className={styles.listItem}>
-          <NavLink onClick = { familyRenderAnotherLinks } className={styles.navLink} to="/home">
+        <li className={home?[styles.listItem, styles.active].join(' '): styles.listItem}>
+          <NavLink  onClick = { familyRenderAnotherLinks } className={styles.navLink} to="/home">
             <img className={styles.itemImg} src={pin} alt="pin" />
             <span>
               Звички i задачi
             </span>
           </NavLink>
         </li>
-        <li className={styles.listItem}>
+        <li className={gifts?[styles.listItem, styles.active].join(' ') :styles.listItem}>
           <NavLink onClick = { familyRenderAnotherLinks } className={styles.navLink} to="/home/gifts">
             <img className={styles.itemImg} src={gift} alt="gift" />
             <span>
