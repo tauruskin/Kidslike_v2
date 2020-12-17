d5722345f59de03ea1563dd3529f1562729e6be0
ihor <malishkoio@gmail.com>
2020-12-17 16:41:59 +0200

    fix

--- client/src/components/UIcomponents/MoreButton/MoreButton.js
+++ client/src/components/UIcomponents/MoreButton/MoreButton.js
@@ -1,20 +1,33 @@
 import React, { useState } from 'react';
-import BubbleComponent from '../BubbleComponent/BubbleComponent'
+import BubbleComponent from '../BubbleComponent/BubbleComponent';
 
-import styles from './MoreButton.module.css'
+import styles from './MoreButton.module.css';
 
-function MoreButton({type, data}) {
-    const [showDropDown, setShowDropDown] = useState(false);
-    const close = () => {
-        setShowDropDown(!showDropDown);
-    };
+function MoreButton({ type, data }) {
+  const [showDropDown, setShowDropDown] = useState(false);
+  const close = () => {
+    setShowDropDown(!showDropDown);
+  };
 
-    return (
-        <div className={styles.drop_down_folder}>
-            <button className={styles.btn} type='button' onClick={() => close()}></button>
-            {showDropDown && <BubbleComponent modalType={type} handleClick={() => close()} taskData={data} habitData={data} giftData={data} childtData={data}/>}
-        </div>
-    )
+  return (
+    <div className={styles.drop_down_folder}>
+      <button
+        className={styles.btn}
+        type="button"
+        onClick={() => close()}
+      ></button>
+      {showDropDown && (
+        <BubbleComponent
+          modalType={type}
+          handleClick={() => close()}
+          taskData={data}
+          habitData={data}
+          giftData={data}
+          childtData={data}
+        />
+      )}
+    </div>
+  );
 }
 
 export default MoreButton;
