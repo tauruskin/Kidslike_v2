e446fab24e90d97a4bc955761a85991f8112d6a8
Koval <dmytrokovalskyi@yahoo.com>
2020-12-17 20:46:43 +0200

    buttons fixed

--- client/src/components/ChildTaskPage/ChildTaskPage.js
+++ client/src/components/ChildTaskPage/ChildTaskPage.js
@@ -13,7 +13,7 @@ export default function ChildTaskPage() {
   const tasks = useSelector(state => state.tasks);
   const children = useSelector(state => state.children);
   // let filteredData = tasksDrow.sort(sortFunc('updatedAt'));
-
+console.log(tasksDrow)
   const dispatch = useDispatch();
   const childId = window.location.href.slice(
     window.location.href.lastIndexOf('/') + 1,
@@ -67,19 +67,20 @@ export default function ChildTaskPage() {
                 <ul className={styles.HabitList}>
                   {tasksDrow.map(
                     element =>
+                      
                       element.childId === el._id &&
                       element.isCompleted === "true" && (
                         <li
                           key={element._id}
                           className={styles.HabitItem}
                           style={
-                            element.isCompleted
+                            element.isCompleted === "true"
                               ? { border: '1px solid rgb(126, 242, 157)' }
                               : { border: '1px solid rgb(235, 162, 185)' }
                           }
                         >
                           {/* <MoreButton type={'task'} /> */}
-                          <TaskItem {...element} repeat={true} />
+                          <TaskItem {...element} />
                         </li>
                       ),
                   )}
@@ -97,3 +98,4 @@ export default function ChildTaskPage() {
     );
   }
 }
+
