9007aacb157e677e1ee68ff4212c5edf8932faf2
Dmytro <tschernomord@gmail.com>
2020-12-17 14:31:11 +0100

    mobile '/'

--- client/src/components/Background/Background.js
+++ client/src/components/Background/Background.js
@@ -3,9 +3,10 @@ import { useLocation } from 'react-router-dom'
 import style from './Background.module.css'
 export default function Background({ children }) {
     const location = useLocation()
-   const publicRoute = location.pathname === '/registration'|| location.pathname === '/login'||location.pathname === '/'
+    const publicRoute = location.pathname === '/registration' || location.pathname === '/login' 
+    const index = location.pathname === '/'
     return (
-    <div className={  publicRoute ? style.background: ''}>
+        <div className={publicRoute ? style.background : index ? [style.background, style.home].join(' '): ""}>
             {children}
         </div>
     )
