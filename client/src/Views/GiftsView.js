3a6e2c5119c2efd42222ce8a7b585c7321705baa
Elena Shevchenko <elena.kadru@gmail.com>
2020-12-17 12:01:33 +0200

    fixed getcurrentUser bug

--- client/src/Views/GiftsView.js
+++ client/src/Views/GiftsView.js
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 import gift from '../img/header/gift.svg';
 import styles from '../components/Gifts/Gifts.module.css';
-//import GiftCard from '../components/Gifts/GiftCard';
 import Button from '../components/UIcomponents/Button/Button';
 import AddPresent from '../components/modals/addPresent/AddPresent';
 import { GiftList } from '../components/Gifts/GiftList';
+import userOperation from '../redux/user/userOperation';
 
 export default function GiftsView() {
+  const dispatch = useDispatch();
   const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    dispatch(userOperation.getUserInfo());
+  }, []);
+
   const close = () =>  setShowModal(false);
   const open = () => setShowModal(true);
+
   return (
     <>
       <div className={styles.container}>
