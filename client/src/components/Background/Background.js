e189e10c2b26ccbcfcf6d09e4206a93b0bb5d54c
Dmytro <tschernomord@gmail.com>
2020-12-17 16:04:32 +0100

    add fonts

--- client/src/components/Background/Background.js
+++ client/src/components/Background/Background.js
@@ -6,7 +6,7 @@ export default function Background({ children }) {
     const publicRoute = location.pathname === '/registration' || location.pathname === '/login' 
     const index = location.pathname === '/'
     return (
-        <div className={publicRoute ? style.background : index ? [style.background, style.home].join(' '): ""}>
+        <div className={publicRoute ? [style.background, style.fonts].join(' ') : index ? [style.background, style.home, style.fonts].join(' '): style.fonts}>
             {children}
         </div>
     )
