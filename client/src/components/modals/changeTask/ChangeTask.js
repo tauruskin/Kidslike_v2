18d73156e888ebba5a3ce2054aada8193010d90d
Nataliia <n3v3st4@gmail.com>
2020-12-18 21:06:40 +0200

    code clean-up

--- client/src/components/modals/changeTask/ChangeTask.js
+++ client/src/components/modals/changeTask/ChangeTask.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
 import FormikControl from '../formik/FormikControl'
@@ -53,28 +53,7 @@ const ChangeTask = ({ close, data }) => {
     }
   }
 
-  // const [taskName, setTaskName] = useState(data.name);
-  // const [mark, setMark] = useState(data.points);
-  // const [taskTarget, setTaskTarget] = useState(data.childId);
-  // const [taskDays, setTaskDays] = useState(data.daysToComplete);
 
-  // const handleSubmit = async evt => {
-  //   evt.preventDefault();
-  //   const result = await dispatch(
-  //     operations.updateTask(
-  //       {
-  //         name: taskName,
-  //         childId: taskTarget,
-  //         points: mark,
-  //         daysToComplete: taskDays,
-  //       },
-  //       data._id,
-  //     ),
-  //   );
-  //   if (result) {
-  //     close();
-  //   }
-  // };
   const handleDelete = () => {
     dispatch(operations.deleteTask(data._id));
     close();
@@ -146,82 +125,6 @@ const ChangeTask = ({ close, data }) => {
           className={styles.modalCloseBtn}
         ></button>
       </div>
-
-
-      {/* <div className={styles.modalBody}>
-        <h2 className={styles.title}>Редагування задачі</h2>
-        <form className={styles.form}>
-          <div className={styles.inputBlock}>
-            <label className={styles.label}>
-              <p className={styles.inputName}>Назва</p>
-              <input
-                defaultValue={data.name}
-                className={styles.input}
-                onChange={({ target: { value } }) => setTaskName(value)}
-                placeholder="Введіть назву"
-              ></input>
-            </label>
-            <label className={styles.label}>
-              <p className={styles.inputName}>Призначення звички</p>
-              <select
-                defaultValue={data.childId}
-                value={children.childId}
-                className={styles.select}
-                onChange={({ target: { value } }) => setTaskTarget(value)}
-                placeholder="Оберіть дитину"
-              >
-                <option key={children.id}>Оберіть дитину</option>
-                {children &&
-                  children.map(child => (
-                    <option key={child._id} value={child._id}>
-                      {child.name}
-                    </option>
-                  ))}
-              </select>
-            </label>
-            <label className={styles.label}>
-              <p className={styles.inputName}>Бал</p>
-
-              <input
-                defaultValue={data.points}
-                className={styles.inputMark}
-                onChange={({ target: { value } }) => setMark(value)}
-                placeholder="__"
-              ></input>
-            </label>
-            <label className={styles.label}>
-              <p className={styles.inputName}>
-                Дні на виконання (необов’язково)
-              </p>
-
-              <input
-                defaultValue={data.daysToComplete}
-                className={styles.inputDays}
-                onChange={({ target: { value } }) => setTaskDays(value)}
-                placeholder="___"
-              ></input>
-            </label>
-            <button className={styles.btnDelete} onClick={() => handleDelete()}>
-              <span className={styles.btnDeleteIcon}></span> Видалити задачу
-            </button>
-          </div>
-          <div className={styles.buttonsBlock}>
-            <button onClick={handleSubmit} className={styles.buttonSave}>
-              {!loaderTask && <span>Зберегти</span>}
-              {loaderTask && <LoaderSmall />}
-            </button>
-
-            <button className={styles.buttonCancle} onClick={() => close()}>
-              Відміна
-            </button>
-          </div>
-          {errorTask && <span>Ops ... err={errorTask}</span>}
-        </form>
-        <button
-          onClick={() => close()}
-          className={styles.modalCloseBtn}
-        ></button>
-      </div> */}
     </>
   );
 };
